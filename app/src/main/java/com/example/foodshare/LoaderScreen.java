67a6fb5ef25673b7aacac56547eeb96a4f0975e7
Francisco Rodríguez Almonacid <franciscorodrigal@gmail.com>
2023-11-16 13:47:13 -0300

    agrega splash screen

---
+++ app/src/main/java/com/example/foodshare/LoaderScreen.java
@@ -0,0 +1,26 @@
+package com.example.foodshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LoaderScreen extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000; // segundos de espera
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loader_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LoaderScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
